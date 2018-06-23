f06ff11242f19307a32407cac8a8a2a13cecc29f
guaneec <guaneec@gmail.com>
2018-06-23 20:00:19 +0800

    Add basic table

---
+++ build.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from jinja2 import Template
+
+
+# analyze
+df = pd.read_json('./spy.json', orient='index')
+df['rating'] = df['positive'] / (df['positive'] + df['negative'])
+df['score'] = (df.median_forever * df.average_forever) ** 0.5 * 100 / 60 / df.price * df.rating
+pdf = df[
+    (df.positive + df.negative >= 500) 
+    & (df.price > 0)].sort_values('score', ascending=False)
+
+# write
+with open('template.html') as ifs:
+    template_str = ifs.read()
+
+template = Template(template_str)
+with open('index.html', 'wb') as ofs:
+    ofs.write(template.render(df=pdf).encode('utf-8'))
